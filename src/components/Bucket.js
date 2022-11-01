e7bf7d69c29aecc7c788ade3341df064ef9940a7
Henry Winget <=>
2022-10-31 21:29:40 -0500

    new changes

---
+++ src/components/Bucket.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import BucketForm from './BucketForm';
+
+function Bucket(props) {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: '',
+    eagerness: '',
+  });
+
+  console.log(props.bucket);
+
+  const submitUpdate = (value) => {
+
+    // TODO: Write logic to update the `edit` value in state after a user updates an entry in the list
+    props.editBucketItem(edit.id, value);
+    setEdit({
+      id: null,
+      value: '',
+      eagerness: '',
+    })
+    // TODO: Set the key:value pairs in the `edit` object back to empty strings
+
+  };
+
+  // If the user is attempting to edit an item, render the bucket form with the edit variable passed as a prop
+  if (edit.id) {
+    return <BucketForm edit={edit} onSubmit={submitUpdate} />;
+  }
+
+  return props.bucket.map((item, index) => (
+    // TODO: Add a className of `bucket row complete ${item.eagerness}` for completed items, and `bucket-row ${item.eagerness}` for non-completed items
+    // TODO: Add a key attribute set to the value of the index position
+    // Hint: use a ternary operator
+    <div className={item.isComplete ? `bucket row complete ${item.eagerness}` : `bucket-row ${item.eagerness}`} key={`bucket-item-${index}`}>
+
+      <div onClick={() => props.completeBucketItem(item.id)}>
+        {item.text}
+      </div>
+      <div className="icons">
+        <p onClick={() => setEdit({ ...item })}> ✏️</p>
+        {/* TODO: Add an onClick event that will invoke the removeBucketItem method passing in the `item.id` */}
+        <p onClick={() => props.removeBucketItem(item.id)}> 🗑️</p>
+      </div>
+    </div>
+  ));
+}
+
+export default Bucket;
